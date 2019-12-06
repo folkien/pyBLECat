fcca5a678fda51956cb3abd04929903ac20a6409
Sławomir Paszko <folkus@gmail.com>
2019-12-06 10:36:10 +0100

    Added installator.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+sudo apt-get -yf install python-pip libglib2.0-dev
+sudo pip install bluepy
+sudo ln -sf ${DIR}/pyBLECat.py /usr/bin/pyBLECat
